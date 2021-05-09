a558334ce7dc9c1dbc00e422dc9eb267ed017c1f
yuanhuihai <yuanhuihai@sina.com>
2021-05-09 16:21:23 +0800

    添加项目文件。

---
+++ IpSearch/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace IpSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.onlineList = new System.Windows.Forms.ListBox();
+            this.offlineList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // onlineList
+            // 
+            this.onlineList.FormattingEnabled = true;
+            this.onlineList.ItemHeight = 12;
+            this.onlineList.Location = new System.Drawing.Point(21, 27);
+            this.onlineList.Name = "onlineList";
+            this.onlineList.Size = new System.Drawing.Size(207, 412);
+            this.onlineList.TabIndex = 0;
+            // 
+            // offlineList
+            // 
+            this.offlineList.FormattingEnabled = true;
+            this.offlineList.ItemHeight = 12;
+            this.offlineList.Location = new System.Drawing.Point(426, 27);
+            this.offlineList.Name = "offlineList";
+            this.offlineList.Size = new System.Drawing.Size(207, 412);
+            this.offlineList.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.offlineList);
+            this.Controls.Add(this.onlineList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox onlineList;
+        private System.Windows.Forms.ListBox offlineList;
+    }
+}
+
---
+++ IpSearch/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+
+
+namespace IpSearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+           getIPlog();
+        }
+
+         
+        public void getIPlog()
+        {
+
+            //获取本地机器名 
+            string _myHostName = Dns.GetHostName();
+            //获取本机IP 
+            string _myHostIP = Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault<IPAddress>(a => a.AddressFamily.ToString().Equals("InterNetwork")).ToString();
+            //MessageBox.Show(_myHostIP);
+            //截取IP网段
+            string ipDuan = _myHostIP.Remove(_myHostIP.LastIndexOf('.'));
+            //MessageBox.Show(ipDuan);
+            //枚举网段计算机
+            Ping myPing = new Ping();
+            string data = "";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            for (int i = 1; i <= 10; i++)
+            {
+                string pingIP = ipDuan + "." + i.ToString();
+                PingReply pingReply = myPing.Send(pingIP, 120, buffer);
+                if (pingReply.Status == IPStatus.Success)
+                {
+                      onlineList.Items.Add("答复的主机地址：" +pingIP + "往返时间：" + pingReply.RoundtripTime.ToString());
+                }
+                else
+                {            
+                    offlineList.Items.Add(pingIP + "不在线");
+                }
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
